f2184990e61bfe66d9fa0b64ee715b056f792b1f
Rianna <rriojas33@student.alamo.edu>
2018-09-20 17:14:06 -0500

    9/20/18

--- rubric4.js
+++ rubric4.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // JavaScript Document
 
 
@@ -22,28 +22,10 @@ var myColors = ["green","orange","purple"];
 alert ("Yellow and Blue make "+ myColors[0]);
 alert ("Yellow and Red make "+ myColors[1]);
 alert ("Red and Blue make "+ myColors[2]);
-=======
-// JavaScript Document
-
-
-var myElement = document.getElementById("second");
-
-console.log (myElement.childNodes.length);
 
 
 
-var myTag =document.getElementsByTagName("li");
-console.log("number of primary colors:" +myTag.length);
 
-var newItem = document.createElement("li");
-var newText = document.createTextNode("Secondary colors are the combonation of Primary colors.");
 
-myElement.appendChild(newItem);
-newItem.appendChild(newText);
 
-var myColors = ["green","orange","purple"];
 
-alert ("Yellow and Blue make "+ myColors[0]);
-alert ("Yellow and Red make "+ myColors[1]);
-alert ("Red and Blue make "+ myColors[2]);
->>>>>>> 28ec2f4e71d4d451a184df0f5e3828c72fbd1814
