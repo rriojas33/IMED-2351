de7505953735d6b2172b9ea70fa9eaed0dd81424
Rianna <rriojas33@student.alamo.edu>
2018-10-01 11:55:36 -0500

    redo rubric 2

---
+++ rubric2js 2.js
@@ -0,0 +1,38 @@
+// JavaScript Document// JavaScript Document
+
+function multiplyNumbers (a ,b)
+{var multiplyTotal = a *b;
+return multiplyTotal;}
+
+var multiplyNumbers2 = multiplyNumbers (3,11);
+alert (multiplyNumbers2);
+	
+var firstName = prompt ("What is your name?");
+
+
+var firstName =(firstName); {
+	alert ("Welcome," + firstName);
+    }
+
+
+var favanimal = prompt ("What is your favorite animal?");
+	
+    
+switch (favanimal){
+	case "cat":
+	alert ("Mine Too!!!");
+	break;
+		
+	case "dog":
+	alert ("Dogs are awesome!!");
+	break;
+		
+	default:
+	alert ("Wow, that super cool!");
+}
+
+
+
+
+	
+	
